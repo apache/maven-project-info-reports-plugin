2d81a9739c93a3286ffe97d23b64dc7805d95e12
Slawomir Jaranowski <s.jaranowski@gmail.com>
2020-12-27 12:08:42 +0100

    [MPIR-391] Continuous Integration support for GitHub Actions
    This closes #27

--- src/main/java/org/apache/maven/report/projectinfo/CiManagementReport.java
+++ src/main/java/org/apache/maven/report/projectinfo/CiManagementReport.java
@@ -98,6 +98,7 @@ public class CiManagementReport
                 "buildforge",
                 "continuum",
                 "cruisecontrol",
+                "github",
                 "hudson",
                 "jenkins",
                 "luntbuild",
